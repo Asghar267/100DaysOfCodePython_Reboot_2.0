caba6c408978a956e4aaf4b2f709ed468cfc57a6
Asghar Abbasi <asgharabbasi267@gmail.com>
2022-09-13 02:05:32 +0500

    third commit

---
+++ Day 2.py
@@ -0,0 +1,9 @@
+print("This is Day 2")
+# echo "# 100DaysOfCodePython_Reboot_2.0" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+
+# git remote add origin https://github.com/Asghar267/100DaysOfCodePython_Reboot_2.0.git
+# git branch -M main
+# git push -u origin main or git push origin main --force
